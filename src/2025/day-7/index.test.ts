45c511329de8bcff082ae0ddaba83557186905cc
Kitketovsky <kitketovsky@gmail.com>
2025-12-07 15:00:00 +0400

    day 7 2025 part 1 non mutational way

--- src/2025/day-7/index.test.ts
+++ src/2025/day-7/index.test.ts
@@ -1,10 +1,10 @@
 import { describe, expect, test } from 'vitest'
 
-import { getTachyonBeamDispersionManifold, parseToManifold } from '.'
+import { getTachyonBeamSplitTimes } from '.'
 
 describe('--- Day 7: Laboratories ---', () => {
 	describe('Part 1:', () => {
-		const input = parseToManifold([
+		const input = [
 			'.......S.......',
 			'...............',
 			'.......^.......',
@@ -21,33 +21,10 @@ describe('--- Day 7: Laboratories ---', () => {
 			'...............',
 			'.^.^.^.^.^...^.',
 			'...............',
-		])
-
-		const expected = parseToManifold([
-			'.......S.......',
-			'.......|.......',
-			'......|^|......',
-			'......|.|......',
-			'.....|^|^|.....',
-			'.....|.|.|.....',
-			'....|^|^|^|....',
-			'....|.|.|.|....',
-			'...|^|^|||^|...',
-			'...|.|.|||.|...',
-			'..|^|^|||^|^|..',
-			'..|.|.|||.|.|..',
-			'.|^|||^||.||^|.',
-			'.|.|||.||.||.|.',
-			'|^|^|^|^|^|||^|',
-			'|.|.|.|.|.|||.|',
-		])
-
-		test('create manifold after dispersion', () => {
-			expect(getTachyonBeamDispersionManifold(input).manifold).toEqual(expected)
-		})
+		]
 
 		test('calculate how many times the beam was split', () => {
-			expect(getTachyonBeamDispersionManifold(input).beamTimesSplit).toBe(21)
+			expect(getTachyonBeamSplitTimes(input)).toBe(21)
 		})
 	})
 })
--- src/2025/day-7/index.ts
+++ src/2025/day-7/index.ts
@@ -1,6 +1,6 @@
 import path from 'node:path'
 
-import { createSyncLineListFromInput } from 'utils/createSyncLineListFromInput'
+import { createSyncLineListFromInput } from './../../utils/createSyncLineListFromInput'
 
 const ENTITIES = {
 	BEAM_START: 'S',
@@ -9,63 +9,55 @@ const ENTITIES = {
 	EMPTY: '.',
 } as const
 
-type Entities = (typeof ENTITIES)[keyof typeof ENTITIES]
-type TachyonManifold = Entities[][]
-
 const inputFilePath = path.resolve('src', '2025', 'day-7', 'input.txt')
 
 const inputLines = createSyncLineListFromInput(inputFilePath)
-const parsedInput = parseToManifold(inputLines)
 
 function main() {
-	console.log(
-		'Part 1:',
-		getTachyonBeamDispersionManifold(parsedInput).beamTimesSplit,
-	)
+	console.log('Part 1:', getTachyonBeamSplitTimes(inputLines))
 }
 
 main()
 
-export function parseToManifold(lines: string[]) {
-	return lines.map((line) => line.split('')) as TachyonManifold
-}
-
-export function getTachyonBeamDispersionManifold(input: TachyonManifold) {
-	const initialMap = structuredClone(input)
-
+export function getTachyonBeamSplitTimes(input: string[]) {
+	let prevState = input[0].split('')
 	let beamTimesSplit = 0
 
-	for (let row = 1; row < initialMap.length; row++) {
-		for (let col = 0; col < initialMap[row].length; col++) {
-			const prevChar = initialMap[row - 1][col]
-			const currChar = initialMap[row][col]
+	for (let row = 1; row < input.length; row++) {
+		const newState = input[row].split('')
+
+		for (let col = 0; col < input[row].length; col++) {
+			const prevChar = prevState[col]
+			const currChar = newState[col]
 
 			if (prevChar === ENTITIES.BEAM_START && currChar === ENTITIES.EMPTY) {
-				initialMap[row][col] = ENTITIES.BEAM
+				newState[col] = ENTITIES.BEAM
 				continue
 			}
 
 			if (prevChar === ENTITIES.BEAM && currChar === ENTITIES.EMPTY) {
-				initialMap[row][col] = ENTITIES.BEAM
+				newState[col] = ENTITIES.BEAM
 				continue
 			}
 
 			if (prevChar === ENTITIES.BEAM && currChar === ENTITIES.SPLITTER) {
-				const leftFromCurr = initialMap[row][col - 1]
-				const rightFromCurr = initialMap[row][col + 1]
+				const leftFromCurr = newState[col - 1]
+				const rightFromCurr = newState[col + 1]
 
 				if (leftFromCurr === ENTITIES.EMPTY) {
-					initialMap[row][col - 1] = ENTITIES.BEAM
+					newState[col - 1] = ENTITIES.BEAM
 				}
 
 				if (rightFromCurr === ENTITIES.EMPTY) {
-					initialMap[row][col + 1] = ENTITIES.BEAM
+					newState[col + 1] = ENTITIES.BEAM
 				}
 
 				beamTimesSplit++
 			}
 		}
+
+		prevState = newState
 	}
 
-	return { manifold: initialMap, beamTimesSplit }
+	return beamTimesSplit
 }
